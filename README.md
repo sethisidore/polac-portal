71d814a36abd833ed5f98df1ca99248e8d848174
Kings Isidore <sethisidore@gmail.com>
2018-07-30 12:14:48 +0100

    Initial Commit: App Resturing into independent modules

---
+++ README.md
@@ -0,0 +1,12 @@
+########################################################################
+
+
+1. All Back-End Application logic is handle in the '/server' folders and Front-End in the '/app' directory
+2. To Start the app in command line:
+    :/>cd server
+    :/> SET DEBUG=portalapp:* & npm start
+
+    for automatic server restart in case of development, use:
+    
+    :/>SET DEBUG=portalapp:* & npm run devstart
+   
---
+++ server/app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+//Routes
+
+const app = express();
+
+//Mongoose Database setup
+const mongoDB = require('./config/database');
+const mongoose = require('mongoose');
+mongoose.connect(mongoDB.url);
+mongoose.Promise = global.Promise;
+let database = mongoose.connection;
+database.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../app')));
+
+//Send Page for angular to make use of
+app.get('*', function(req, res) {
+  res.render('index', {title: 'Academy Portal'});
+});
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
